b9f7f28a88ffca482c42c46f3e1389f01fc5c0c4
Sharifzadeh <sharifzadeh.arman@gmail.com>
2024-02-07 16:02:58 +0330

    021112

--- src/main/java/com/dam/config/SpringSecurityConfig.java
+++ src/main/java/com/dam/config/SpringSecurityConfig.java
@@ -45,7 +45,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 //  .cors().disable()
                 .authorizeRequests()
                 .antMatchers(
-                         "/dam/**",
+               //          "/dam/**",
                         Routes.POST_user_verify_email,
                         Routes.POST_user_verify_mobile,
                         Routes.POST_user_auth_email,
